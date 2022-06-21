79d1a41c6199b9944766a86e389a1c98d6431524
wumpfroot <markuskojo@msn.com>
2022-06-21 15:09:48 +0100

    8th commit - added cors to link it with front end

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("./db/connection")
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 5000;
 const userRouter = require("./user/routes");
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 
 app.listen(port, () => {
